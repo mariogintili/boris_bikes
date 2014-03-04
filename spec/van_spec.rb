6a454eb9fbf80901f38ca5be4dda3804690e7e90
Mario Gintili <mariojgintili@gmail.com>
2014-03-04 14:24:19 +0000

    refactored tests

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -16,11 +16,14 @@ require 'docking_station'
 
 				@working_bike, @broken_bike = Bike.new, Bike.new
 				@broken_bike.break
+				van.dock(@working_bike)
+				van.dock(@broken_bike)
+				van.dock(@broken_bike)
 			end
 
 			it "load broken bikes to the van" do
 				van.dock(@broken_bike)
-				expect(van.broken_bikes.count).to eq(1)
+				expect(van.broken_bikes.count).to eq(3)
 			end
 
 			it "takes only broken bikes to the garage" do
@@ -28,29 +31,25 @@ require 'docking_station'
 				 van.dock(@working_bike)
 				 van.dock(@broken_bike)
 				 van.broken_bikes.each { |bike| fake_garage.bikes << bike  }
-				 expect(fake_garage.broken_bikes.count).to eq(1)
+				 expect(fake_garage.broken_bikes.count).to eq(3)
 			end
 
 			it "takes only fixed bikes to the docking station" do
-				van.dock(@working_bike)
-				van.dock(@working_bike)
-				van.dock(@broken_bike)
 				van.available_bikes.each {|bike| docking_station.bikes << bike }
-				expect(docking_station.available_bikes.count).to eq(2)
+				expect(docking_station.available_bikes.count).to eq(1)
 			end
 
 			it "moves broken bikes to the garage" do
-				van.dock(@working_bike)
-				van.dock(@broken_bike)
-				van.dock(@broken_bike)
 				van.take_broken_bikes_to(garage)
 				expect(garage.broken_bikes.count).to eq(2)
 			end
 
 			it "moves available bikes to docking station" do
-				van.dock(@working_bike)
-				van.dock(@broken_bike)
-				van.dock(@broken_bike)
+				van.take_available_bikes_to(docking_station)
+				expect(docking_station.available_bikes.count).to eq(1)
+			end
+
+			it "moves bikes to destination" do
 				van.take_available_bikes_to(docking_station)
 				expect(docking_station.available_bikes.count).to eq(1)
 			end
