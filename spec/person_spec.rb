d8e5adc775609117fcb6b035d6627b8d5c36742c
Mario Gintili <mariojgintili@gmail.com>
2014-03-06 14:45:54 +0000

    refactored person test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,29 +8,26 @@ require 'docking_station'
 		let (:station) {DockingStation.new}
 		let (:mario) {Person.new}
 
-		it "should take a bike from docking station" do
+		before do
 			station.dock(bike)
 			mario.rent_from(station.available_bikes)
+		end
+
+		it "should take a bike from docking station" do
 			expect(mario.has_bike?).to eq(true) 
 		end
 
 		it "should be able to fall off and break the bike" do
-			station.dock(bike)
-			mario.rent_from(station.available_bikes)
 			mario.fall_down
 			expect(mario.bike_broken?).to eq(true) 
 		end
 
 		it "raises an error if person tries to rent +1 bike" do
 			station.dock(bike)
-			station.dock(bike)
-			mario.rent_from(station.available_bikes)
 			expect { mario.rent_from(station.available_bikes) }.to raise_error(RuntimeError)
 		end
 
 		it "steals a bike from another person" do
-			station.dock(bike)
-			mario.rent_from(station.available_bikes)
 			jordan = Person.new
 			jordan.steal_bike_from(mario)
 			expect(jordan.has_bike?).to eq(true)
